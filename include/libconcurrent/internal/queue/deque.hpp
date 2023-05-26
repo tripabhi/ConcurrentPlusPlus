438c1e25113f0e8d7d1ebeeb552ed99b0bffbfc6
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2023-05-26 02:28:42 +0000

    Added boilerplate for Deque class

--- include/libconcurrent/internal/queue/deque.hpp
+++ include/libconcurrent/internal/queue/deque.hpp
@@ -1,8 +1,16 @@
 #ifndef __DEQUE_HPP__
 #define __DEQUE_HPP__
 
+#include <buffer.hpp>
+#include <optional>
+
 namespace async {
-    
-}
+template <typename T> class Deque {
+  void push(T &lval);
+  void push(T &&rval);
+  std::optional<T> pop() noexcept;
+  std::optional<T> steal() noexcept;
+};
+} // namespace async
 
 #endif
